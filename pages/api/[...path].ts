7c545226da9bb38a9f89ade041f6f8227d0592e1
Admin <admin@Admins-MacBook-Pro.local>
2024-02-19 15:45:25 +0700

    debug proxy with static urrl

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -18,8 +18,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
   // overwrite cookie
   // req.headers.cookie = "";
 
-  proxy.web(req, res, { target: process.env.API_URL, changeOrigin: true, selfHandleResponse: false });
-  // proxy.web(req, res, { target: "https://js-post-api.herokuapp.com", changeOrigin: true, selfHandleResponse: false });
+  // proxy.web(req, res, { target: process.env.API_URL, changeOrigin: true, selfHandleResponse: false });
+  proxy.web(req, res, { target: "https://js-post-api.herokuapp.com", changeOrigin: true, selfHandleResponse: false });
 
   // res.status(200).json({ name: "PATH - Match all here" });
 }
