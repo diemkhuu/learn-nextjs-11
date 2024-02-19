1332815296c5966dcc6a49bca4f69c71370434e3
Admin <admin@Admins-MacBook-Pro.local>
2024-02-19 15:17:25 +0700

    update type proxy

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -2,16 +2,18 @@
 import httpProxy from "http-proxy";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
+// type Data = {
+//   name: string;
+// };
 
 var proxy = httpProxy.createProxyServer();
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   // overwrite cookie
   req.headers.cookies = "";
 
   proxy.web(req, res, { target: process.env.API_URL, changeOrigin: true, selfHandleResponse: false });
   // proxy.web(req, res, { target: "https://js-post-api.herokuapp.com", changeOrigin: true, selfHandleResponse: false });
+
+  // res.status(200).json({ name: "PATH - Match all here" });
 }
