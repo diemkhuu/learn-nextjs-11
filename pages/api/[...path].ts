9903c2ecf784c71baabd45c4b1bfdc14b1c6dea5
Admin <admin@Admins-MacBook-Pro.local>
2024-02-19 16:10:21 +0700

    revert code URL variable

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -18,9 +18,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
   // overwrite cookie
   req.headers.cookie = "";
 
-  // proxy.web(req, res, { target: process.env.API_URL, changeOrigin: true, selfHandleResponse: false });
   req.headers["x-forwarded-port"] ||= "80";
-  proxy.web(req, res, { target: "https://js-post-api.herokuapp.com", changeOrigin: true, selfHandleResponse: false });
+  proxy.web(req, res, { target: process.env.API_URL, changeOrigin: true, selfHandleResponse: false });
+  // proxy.web(req, res, { target: "https://js-post-api.herokuapp.com", changeOrigin: true, selfHandleResponse: false });
 
   // res.status(200).json({ name: "PATH - Match all here" });
 }
